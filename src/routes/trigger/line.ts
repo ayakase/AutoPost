039f9ea1eedbc61bf633edcd616cbb64e4fa6e0d
ayakase <wibu1892001@gmail.com>
2025-08-06 14:32:36 +0700

    [add] test workflow automation

--- src/routes/trigger/line.ts
+++ src/routes/trigger/line.ts
@@ -2,7 +2,6 @@
 import { Hono } from 'hono'
 import { handleLineWebhook } from '../../api-handlers/line'
 const lineRoute = new Hono()
-// used once just to get user ID
 lineRoute.post('/line', async (c) => {
     return handleLineWebhook(c)
 })
